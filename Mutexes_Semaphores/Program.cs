0108ac6b192a6c89c6a4f2708699f4ebc356e0d3
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 12:47:37 +0300

    Ex 5 was done

--- Mutexes_Semaphores/Program.cs
+++ Mutexes_Semaphores/Program.cs
@@ -1,9 +1,41 @@
 ﻿namespace Mutexes_Semaphores
 {
-    public class Program
+    class Program
     {
+        static SemaphoreSlim semaphore = new SemaphoreSlim(3);
+
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 10; i++)
+            {
+                int threadId = i;
+                Thread thread = new Thread(() => DoWork(threadId));
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void DoWork(int threadId)
+        {
+            semaphore.Wait(); 
+
+            try
+            {
+                Console.WriteLine($"Thread {threadId} started.");
+
+                Random random = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine($"Thread {threadId} output: {random.Next(100)}");
+                }
+
+                Console.WriteLine($"Thread {threadId} completed.");
+            }
+            finally
+            {
+                semaphore.Release(); 
+            }
         }
     }
 }
