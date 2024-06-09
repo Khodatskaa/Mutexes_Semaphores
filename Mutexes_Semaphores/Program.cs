0fa1882c97b4d5b9eb99dcf8a973f3a168f5c4a4
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 12:37:02 +0300

    Ex 2 was

--- Mutexes_Semaphores/Program.cs
+++ Mutexes_Semaphores/Program.cs
@@ -1,9 +1,49 @@
 ﻿namespace Mutexes_Semaphores
 {
-    public class Program
+    class Program
     {
+        private static Mutex _mutex = new Mutex();
+        private static int[] _dataArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        private const int ArbitraryNumber = 5; 
+
         static void Main(string[] args)
         {
+            Task.Run(() => ModifyArray());
+            Task.Run(() => FindMaxValue());
+
+            Console.ReadLine();
+        }
+
+        private static void ModifyArray()
+        {
+            _mutex.WaitOne();
+            try
+            {
+                for (int i = 0; i < _dataArray.Length; i++)
+                {
+                    _dataArray[i] += ArbitraryNumber;
+                    Console.WriteLine($"Modified element {i}: {_dataArray[i]}");
+                }
+                Console.WriteLine("Array modification completed.");
+            }
+            finally
+            {
+                _mutex.ReleaseMutex();
+            }
+        }
+
+        private static void FindMaxValue()
+        {
+            _mutex.WaitOne();
+            try
+            {
+                int maxValue = _dataArray.Max();
+                Console.WriteLine($"Maximum value in the array: {maxValue}");
+            }
+            finally
+            {
+                _mutex.ReleaseMutex();
+            }
         }
     }
 }
