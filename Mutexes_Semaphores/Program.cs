8620cfc2135e7f691981e3c46a3fade791aa6c94
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 12:46:12 +0300

    Ex 4 was done

--- Mutexes_Semaphores/Program.cs
+++ Mutexes_Semaphores/Program.cs
@@ -1,9 +1,33 @@
 ﻿namespace Mutexes_Semaphores
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            string mutexName = "SingleInstanceMutex";
+            Mutex? mutex = null;
+            bool createdNew = false;
+
+            try
+            {
+                mutex = new Mutex(true, mutexName, out createdNew);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return;
+            }
+
+            if (createdNew)
+            {
+                Console.WriteLine("Application started successfully. Press any key to exit.");
+                Console.ReadKey();
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
