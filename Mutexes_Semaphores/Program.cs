d5f06ee68f12a833a542a68254b0a14aff4d0f87
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-09 12:40:36 +0300

    Ex 1 was done

--- Mutexes_Semaphores/Program.cs
+++ Mutexes_Semaphores/Program.cs
@@ -1,9 +1,49 @@
 ﻿namespace Mutexes_Semaphores
 {
-    public class Program
+    class Program
     {
+        private static Mutex _mutex = new Mutex();
+
         static void Main(string[] args)
         {
+            Task thread1 = Task.Run(() => DisplayAscendingNumbers());
+            Task thread2 = Task.Run(() => DisplayDescendingNumbers());
+
+            Task.WaitAll(thread1, thread2);
+        }
+
+        private static void DisplayAscendingNumbers()
+        {
+            _mutex.WaitOne(); 
+            try
+            {
+                for (int i = 0; i <= 20; i++)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(100); 
+                }
+            }
+            finally
+            {
+                _mutex.ReleaseMutex(); 
+            }
+        }
+
+        private static void DisplayDescendingNumbers()
+        {
+            _mutex.WaitOne(); 
+            try
+            {
+                for (int i = 10; i >= 0; i--)
+                {
+                    Console.WriteLine(i);
+                    Thread.Sleep(100); 
+                }
+            }
+            finally
+            {
+                _mutex.ReleaseMutex(); 
+            }
         }
     }
 }
